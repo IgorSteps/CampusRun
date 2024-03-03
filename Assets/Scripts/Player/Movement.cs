7db68783492e2cb41e70f92642c9a2bae1406480
IgorSteps <igorstepanenko696@gmail.com>
2024-03-03 19:11:22 +0000

    Right left movements

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -7,10 +5,30 @@ public class Movement : MonoBehaviour
     public float Speed; 
     void Start()
     {
+        Debug.Log("Left boundary: " + Constants.LEFT_BOUNDARY + "\n " + "Right boundary:" + Constants.RIGHT_BOUNDARY);
     }
 
     void Update()
     {
+        movePlayer();
+    }
+
+    private void movePlayer()
+    {
+        // Fowards.
         transform.Translate(Speed * Time.deltaTime * Vector3.forward, Space.World);
+
+        // Sideways.
+        if (Input.GetKey(KeyCode.A))
+        {
+            if(this.gameObject.transform.position.x > Constants.LEFT_BOUNDARY)
+                transform.Translate(3.0f * Time.deltaTime * Vector3.left);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (this.gameObject.transform.position.x < Constants.RIGHT_BOUNDARY)
+                transform.Translate(3.0f * Time.deltaTime * Vector3.right);
+        }
     }
 }
