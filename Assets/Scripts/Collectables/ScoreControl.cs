9560b79cedf94f25369169ea6afe5338a8ed13aa
IgorSteps <igorstepanenko696@gmail.com>
2024-03-05 00:51:19 +0000

    Cleanup

--- Assets/Scripts/Collectables/ScoreControl.cs
+++ Assets/Scripts/Collectables/ScoreControl.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 public class ScoreControl : MonoBehaviour
 {
     public static int s_Score = 0;
-    public GameObject ScoreText;
+    public GameObject ScoreNumber;
 
     // Update is called once per frame
     void Update()
     {
-        ScoreText.GetComponent<TextMeshProUGUI>().text = s_Score.ToString();
+        ScoreNumber.GetComponent<TextMeshProUGUI>().text = s_Score.ToString();
     }
 }
