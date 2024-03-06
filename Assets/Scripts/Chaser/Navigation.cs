572f7634e9e24be1bb417cb499f0988e794731f1
IgorSteps <igorstepanenko696@gmail.com>
2024-03-06 00:24:40 +0000

    Cleanup

--- Assets/Scripts/Chaser/Navigation.cs
+++ Assets/Scripts/Chaser/Navigation.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,13 +5,12 @@ public class Navigation : MonoBehaviour
 {
     public Transform Player;
     private NavMeshAgent Agent;
-    // Start is called before the first frame update
+
     void Start()
     {
         Agent = this.gameObject.GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Agent.destination = Player.position;
