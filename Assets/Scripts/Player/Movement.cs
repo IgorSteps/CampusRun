5c17fb4d999bfe3f45c66164f6e3b81ea54fd425
IgorSteps <igorstepanenko696@gmail.com>
2024-04-19 13:29:24 +0100

    Use new input system

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,31 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     public float FrontSpeed = Constants.DEFAULT_FORWARD_SPEED;
     public float SideSpeed = Constants.DEFAULT_SIDE_SPEED;
 
+    private Vector2 _movement;
+
     void Update()
     {
-        movePlayer();
+        // Forward movement
+        transform.Translate(FrontSpeed * Time.deltaTime * Vector3.forward, Space.World);
+        
+        // Side movement.
+        if (_movement.x < 0 && this.gameObject.transform.position.x > Constants.LEFT_BOUNDARY)
+        {
+            transform.Translate(SideSpeed * Time.deltaTime * Vector3.left);
+        }
+        else if (_movement.x > 0 && this.gameObject.transform.position.x < Constants.RIGHT_BOUNDARY)
+        {
+            transform.Translate(SideSpeed * Time.deltaTime * Vector3.right);
+        }
     }
 
-    private void movePlayer()
+    // OnMove updates the movement variable based on the InputAction.
+    public void OnMove(InputValue value)
     {
-        // Fowards.
-        transform.Translate(FrontSpeed * Time.deltaTime * Vector3.forward, Space.World);
-
-        // Sideways.
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            if(this.gameObject.transform.position.x > Constants.LEFT_BOUNDARY)
-                transform.Translate(SideSpeed * Time.deltaTime * Vector3.left);
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            if (this.gameObject.transform.position.x < Constants.RIGHT_BOUNDARY)
-                transform.Translate(SideSpeed * Time.deltaTime * Vector3.right);
-        }
+        _movement = value.Get<Vector2>();
+        Debug.Log("Move input received: " + _movement);
     }
 }
