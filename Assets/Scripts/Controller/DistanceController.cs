6e8362ca3f1ca648079ab3a5fb61e6ca43f0cf75
IgorSteps <igorstepanenko696@gmail.com>
2024-04-28 18:53:00 +0100

    Better HUD: distance display and better visible

---
+++ Assets/Scripts/Controller/DistanceController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _distanceText;
+    [SerializeField] private Transform _playerTransform;
+    private Vector3 _startPosition;
+    private float _distanceTravelled = 0f;
+
+    void Start()
+    {
+        _startPosition = _playerTransform.position;
+    }
+
+    void Update()
+    {
+        _distanceTravelled = _playerTransform.position.z - _startPosition.z;
+        _distanceText.text = ((int)_distanceTravelled).ToString("D6"); // format as 000000.
+    }
+}
