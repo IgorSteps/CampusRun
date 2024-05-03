936354bddb5351e4883123a6a2b666b62ce0f7f5
IgorSteps <igorstepanenko696@gmail.com>
2024-05-03 17:53:14 +0100

    Jumping

--- Assets/Scripts/Obstacles/Collision.cs
+++ Assets/Scripts/Obstacles/Collision.cs
@@ -8,18 +8,52 @@ public class Collision : MonoBehaviour
     private ShowEndScreen _endScreen;
     private Movement _playerMovement;
     private Animator _playerAnimator;
-
+    [SerializeField] private GameObject _player;
     public void Start()
     {
         _endScreen = GameObject.Find("Screen").GetComponent<ShowEndScreen>();
         _playerMovement = GameObject.Find("Player").GetComponent<Movement>();
         _playerAnimator = GameObject.Find("Aj@Running").GetComponent<Animator>();
+        _player = GameObject.Find("Player");
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        _playerMovement.enabled = false; // stop Player from moving.
-        _playerAnimator.Play("Stumble Backwards"); // play stumble animation.
-        _endScreen.enabled = true; // show end screen.
+        float heigthDiff = other.transform.position.y - this.gameObject.transform.position.y;
+        if (heigthDiff > 1.5f)
+        {
+             UpdateYPositionOnGround();
+        }
+        else
+        {
+            _playerMovement.enabled = false; // stop Player from moving.
+            _playerAnimator.Play("Stumble Backwards"); // play stumble animation.
+            _endScreen.enabled = true; // show end screen.
+        }
     }
+
+    public LayerMask groundLayer;
+
+    void UpdateYPositionOnGround()
+    {
+        _player.transform.position = new Vector3(
+            _player.transform.position.x,
+            this.gameObject.transform.position.y + 4.0f,
+            _player.transform.position.z
+            );
+        Debug.Log("On top of the obstacle, y pos is " + _player.transform.position.y);
+    }
+
+    //void CheckForSideCollisions()
+    //{
+    //    RaycastHit hit;
+    //    // Raycast forward to detect obstacles in the direction of movement
+    //    if (Physics.Raycast(transform.position, Vector3.forward, out hit, 1.0f, groundLayer)) // 1f is the lookahead distance, adjust as necessary
+    //    {
+    //        if (hit.collider.CompareTag("Obstacle"))
+    //        {
+    //            Debug.Log("Dead");
+    //        }
+    //    }
+    //}
 }
--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -5,13 +5,21 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float _frontSpeed = Constants.DEFAULT_PLAYER_FORWARD_SPEED;
     [SerializeField] private float _sideSpeed = Constants.DEFAULT_PLAYER_SIDE_SPEED;
-    private int _currentLane = 1; // 0 = Left, 1 = Middle, 2 = Right
-    private readonly float[] _lanePositions = { -3f, 0f, 3f };
+    [SerializeField] private float _upSpeed;
 
     public GameObject _playerModel;
     private Animator _playerAnimator;
-    
+    [SerializeField] private CharacterController _characterController;
+
+    // For moving.
+    private int _currentLane = 1; // 0 = Left, 1 = Middle, 2 = Right
+    private readonly float[] _lanePositions = { -3f, 0f, 3f };
     private Vector2 _movementInput;
+    private float jumpHeight = 1.0f;
+    private float _gravity = 10f;
+    private bool _isGrounded;
+
+    public LayerMask groundLayer;
 
     void Start()
     {
@@ -20,22 +28,52 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        // Forward movement
-        transform.Translate(_frontSpeed * Time.deltaTime * Vector3.forward, Space.World);
+        _isGrounded = IsGrounded();
+        // Always apply gravity.
+        if (!_isGrounded || _upSpeed < 0)
+        {
+            _upSpeed -= _gravity * Time.deltaTime * 2.5f ;
+        }
+
+        // Forward movement.
+        Vector3 forwardMovement = _frontSpeed * Time.deltaTime * Vector3.forward;
+
+        // Jump movement.
+        Vector3 jumpMovement = _upSpeed * Time.deltaTime * Vector3.up;
 
         // Side movement.
-        Vector3 newPosition = transform.position;
-        // Smooth transition to lane position
-        newPosition.x = Mathf.Lerp(newPosition.x, _lanePositions[_currentLane], Time.deltaTime * _sideSpeed); 
-        transform.position = newPosition;
+        float targetXPosition = _lanePositions[_currentLane];
+        float newXPosition = Mathf.Lerp(transform.position.x, targetXPosition, Time.deltaTime * _sideSpeed);
+        Vector3 sideMovement = new Vector3(newXPosition - transform.position.x, 0, 0);
+
+        // Combine.
+        Vector3 finalMovement = forwardMovement + sideMovement + jumpMovement;
+        _characterController.Move(finalMovement);
+    }
+
+    bool IsGrounded()
+    {
+        // Starts at the center of the player and point down
+        RaycastHit hit;
+        if (Physics.Raycast(
+            transform.position,
+            Vector3.down,
+            out hit,
+            (_characterController.height*2.0f) + 0.1f,
+            groundLayer
+        ))
+        {
+            return true;
+        }
+        return false;
     }
 
     // OnMove updates the lane choice based on the InputAction
     public void OnMove(InputValue value)
     {
         _movementInput = value.Get<Vector2>();
-        Debug.Log("movement y= "+ _movementInput.y);
         int newLane = _currentLane;
+
         if (_movementInput.x < 0) // Move Left
         {
             if (newLane > 0)
@@ -47,8 +85,10 @@ public class Movement : MonoBehaviour
             if (newLane < 2)
                 newLane++;
         } 
-        else if (_movementInput.y > 0) // Jump
+        else if (_movementInput.y > 0 && _isGrounded) // Jump
         {
+            // Fancy maths to find speed required to reach jumpHeight using Earth's gravity.
+            _upSpeed = Mathf.Sqrt(2.0f * _gravity * jumpHeight);
             _playerAnimator.SetTrigger("OnJump");
         }
 
@@ -56,8 +96,6 @@ public class Movement : MonoBehaviour
         if (newLane != _currentLane)
         {
             _currentLane = newLane;
-            // TODO: Figure out how to sync Jump animation and lane movements.
-            //_playerAnimator.SetTrigger("OnJump");
         }
     }
 }
