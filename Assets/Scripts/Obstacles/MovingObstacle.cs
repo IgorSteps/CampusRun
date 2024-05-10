9e716f434b26d7693aaeb35a648e3318164c6f82
IgorSteps <igorstepanenko696@gmail.com>
2024-05-10 10:26:22 +0100

    Moving cars on distance condition

--- Assets/Scripts/Obstacles/MovingObstacle.cs
+++ Assets/Scripts/Obstacles/MovingObstacle.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class MovingObstacle : MonoBehaviour
 {
     [SerializeField] float _speed = 6.0f;
+    [SerializeField] float _moveOffset = 20.0f;
     private ShowEndScreen _endScreen;
     private Movement _playerMovement;
     private Animator _playerAnimator;
+    private GameObject _player;
 
     public void Start()
     {
         _endScreen = GameObject.Find("Screen").GetComponent<ShowEndScreen>();
         _playerMovement = GameObject.Find("Player").GetComponent<Movement>();
         _playerAnimator = GameObject.Find("Aj@Running").GetComponent<Animator>();
+        _player = GameObject.Find("Player");
     }
 
     void Update()
     {
-        transform.Translate(_speed * Time.deltaTime * Vector3.back, Space.World);
+        if (_player.transform.position.z + _moveOffset > gameObject.transform.position.z)
+        {
+            transform.Translate(_speed * Time.deltaTime * Vector3.back, Space.World);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
